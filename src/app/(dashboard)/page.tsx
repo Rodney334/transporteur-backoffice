46e58979554c55f4d4a38d1b31eec916ca781aa1
rodney334 <rodneyivan334@gmail.com>
2025-11-22 21:35:38 +0100

    delete page from (dashboard)

--- src/app/(dashboard)/page.tsx
+++
@@ -1,9 +0,0 @@
-// app/(dashboard)/page.tsx
-export default function DashboardPage() {
-  return (
-    <div>
-      <h1 className="text-2xl font-bold mb-4">Tableau de bord</h1>
-      <p>Bienvenue sur votre tableau de bord.</p>
-    </div>
-  );
-}
