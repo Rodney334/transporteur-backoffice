18c191bca7ac82973c8d29699433bf0edbf79d1c
rodney334 <rodneyivan334@gmail.com>
2026-04-20 09:30:28 +0100

    vercel link

--- src/lib/services/auth-service.ts
+++ src/lib/services/auth-service.ts
@@ -120,8 +120,8 @@ export const authService = {
     }
 
     const response = await axios.post<RefreshTokenResponse>(
-      "https://backend.letrans-porteur.com/api/v1",
-      // "https://letransporteur-production.up.railway.app/api/v1", // "https://backend.letrans-porteur.com/api/v1",
+      // "https://backend.letrans-porteur.com/api/v1",
+      "https://letransporteur-production.up.railway.app/api/v1", // "https://backend.letrans-porteur.com/api/v1",
       { refreshToken },
     );
     return response.data;
